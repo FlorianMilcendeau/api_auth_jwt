ee7a386ef9773c300977229d36ff692aa969cf62
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-01-18 17:31:40 +0100

    Addition: error handling of the create and findOne method

--- models/CrudDao.js
+++ models/CrudDao.js
@@ -16,6 +16,16 @@ class CrudDao {
    * @return {promise} A promise to be resolved with the SQL query or rejected with an error
    */
   static findOne(data) {
+    // 'data' must be object
+    if (typeof data !== 'object') {
+      throw new TypeError('Wrong type given, expected object');
+    }
+
+    // "Data" its length must be 1.
+    if (Object.keys(data).length !== 1) {
+      throw new Error("'Data' its length must contain 1 property");
+    }
+
     return new Promise((resolve, reject) => {
       const sql = `SELECT * FROM ${this.table} WHERE ?`;
 
@@ -40,6 +50,15 @@ class CrudDao {
    * @return {promise} A promise to be resolved with the SQL query or rejected with an error.
    */
   static create(data) {
+    // 'data' must be object
+    if (typeof data !== 'object') {
+      throw new TypeError('Wrong type given, expected object');
+    }
+
+    if (Object.keys(data).length < 1) {
+      throw new Error("'data' its length must contain at least 1 property.");
+    }
+
     return new Promise((resolve, reject) => {
       const sql = `INSERT INTO ${this.table} SET ?`;
 
