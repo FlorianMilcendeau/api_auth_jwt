53503dd0427823a085627159fabe387b3bb7d084
Florian Milcendeau <fmilcendeau85@gmail.com>
2021-01-18 17:29:23 +0100

    Change: connection from singleton to createPool

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,13 @@
 const mysql = require('mysql');
 
-let connection;
-
-const getConnection = () => {
-  // If connection does not exist, I create it.
-  if (!connection) {
-    connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-  }
-
-  // If the 'connection' has disconnected, I connect it.
-  if (connection && connection.state === 'disconnected') {
-    connection.connect((err) => {
-      if (err) {
-        console.error(`Error connecting ${err.stack}`);
-      }
-      console.log(`Database connected as id: ${connection.threadId}`);
-    });
-  }
-
-  return connection;
+const config = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
-module.exports = getConnection();
+const connection = mysql.createPool(config);
+
+module.exports = connection;
