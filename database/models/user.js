ec2777baf0bb952fbeb3026be11c6d71dbc3f57d
Florian Milcendeau <fmilcendeau@listopaye.fr>
2021-05-14 21:43:16 +0200

    Addition: setup - sending the email

--- database/models/user.js
+++ database/models/user.js
@@ -1,38 +1,25 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class user extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-    user.init(
-        {
-            name: { type: DataTypes.STRING(50), allowNull: false },
-            email: {
-                type: DataTypes.STRING(50),
-                allowNull: false,
-                unique: true,
-            },
-            password: { type: DataTypes.STRING(255), allowNull: false },
-            is_admin: { type: DataTypes.BOOLEAN, defaultValue: 0 },
-            created_at: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: new Date(),
-            },
-        },
-        {
-            sequelize,
-            modelName: 'user',
-            underscored: true,
-            timestamps: false,
-        }
-    );
-    return user;
+  }
+  user.init(
+    {
+      name: { type: DataTypes.STRING(50), allowNull: false },
+      email: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+      password: { type: DataTypes.STRING(255), allowNull: false },
+      is_admin: { type: DataTypes.BOOLEAN, defaultValue: 0 },
+      created_at: { type: DataTypes.DATE, allowNull: false, defaultValue: new Date() },
+    },
+    { sequelize, modelName: 'user', underscored: true, timestamps: false }
+  );
+  return user;
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const authenticate = require('../controllers/authenticate');
+const user = require('../controllers/user');
 
 /** Route authentication */
 router.use('/authenticate', authenticate);
+router.use('/user', user);
 
 module.exports = router;
